f1eb745d8061833670654a2d7c32187de437b7e7
sshjerry <defineantoine@gmail.com>
2022-07-29 13:54:35 -0700

    Admin Model Class Persistence

--- src/main/java/com/quickbytes/backend/model/Admin.java
+++ src/main/java/com/quickbytes/backend/model/Admin.java
@@ -1,10 +1,23 @@
 package com.quickbytes.backend.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Admin {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@Column(nullable = false)
 	private String firstName;
+	@Column(nullable = false)
 	private String lastName;
+	@Column(nullable = false)
 	private String username;
+	@Column(nullable = false)
 	private String password;
 	public Admin() {
 		super();
